d7cc3ba49b2684e3e9f5cb94cd7680b508baf3e5
Harsha-vardhan1 <103881981+Harsha-vardhan1@users.noreply.github.com>
2023-09-28 09:14:16 +0530

    Add files via upload

---
+++ Inheriatnce.py
@@ -0,0 +1,24 @@
+class Parent:
+    def gold(self):
+        print("Price 5l")
+    def car(self):
+        print("Price 3L")
+class Baby1(Parent):
+    def bank(warangal):
+        print("Deposited 2L")
+
+class gb(Baby1):
+    def jewels(warangal):
+        print("Price 10L")
+class Baby2(Parent):
+    def me(self):
+        print("Hai")
+b2=gb()
+b2.jewels()
+b2.bank()
+b2.car()
+b2.gold()
+b3=Baby2()
+b3.me()
+b3.gold()
+b3.car()
